afe63aa673fe868b5cba9489d7aa814c120f4525
Brian Kenny <bkenny@gmailcom>
2014-04-19 09:28:11 -0400

    New Lists model created

---
+++ lists/migrations/0003_auto_20140419_1324.py
@@ -0,0 +1,27 @@
+# encoding: utf8
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0002_item_text'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='list',
+            field=models.ForeignKey(to='lists.List', to_field='id', default=None),
+            preserve_default=True,
+        ),
+    ]
